577911a071026049ec16f241d2d27cf8d4a74d8a
VinayProgram <vinaytandale@gmail.com>
2025-02-20 05:06:47 +0000

    distance function added custom

--- distance/src/radial.tsx
+++ distance/src/radial.tsx
@@ -23,7 +23,7 @@ const Radial = () => {
   const human = new THREE.CircleGeometry(2,100)
   const humanMat = new THREE.MeshBasicMaterial({color:'peach'})
   const humanmesh = new THREE.Mesh(human,humanMat)
-  humanmesh.position.set(230,100,0)
+  humanmesh.position.set(100,100,0)
 
   scene.add(humanmesh)
 
@@ -45,7 +45,7 @@ const Radial = () => {
       }
 
       // Calculate Distance
-      const distance = math.distance([...humanmesh.position.toArray()], [...meshCube.position.toArray()]);
+      const distance =eucDistance(meshCube.position,humanmesh.position)
       console.log(distance)
       // Change color if close enough
       if (distance <= 10) {
@@ -63,4 +63,11 @@ const Radial = () => {
   return <div ref={canvas}></div>;
 };
 
+const eucDistance = (v1: THREE.Vector3, v2: THREE.Vector3): number => {
+    return Math.sqrt(
+        Math.pow(v2.x - v1.x, 2) +
+        Math.pow(v2.y - v1.y, 2) +
+        Math.pow(v2.z - v1.z, 2)
+    );
+};
 export default Radial;
